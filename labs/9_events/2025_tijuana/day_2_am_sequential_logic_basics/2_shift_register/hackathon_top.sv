214a8c827d587b7ca2385d0861f3456e04bff5bd
jose313-stack <jose19334@gmail.com>
2025-01-30 00:18:03 -0800

    ejercicio 2 shift register day 2 am

--- labs/9_events/2025_tijuana/day_2_am_sequential_logic_basics/2_shift_register/hackathon_top.sv
+++ labs/9_events/2025_tijuana/day_2_am_sequential_logic_basics/2_shift_register/hackathon_top.sv
@@ -43,22 +43,86 @@ module hackathon_top
 
     logic [7:0] shift_reg;
 
+    /*
     always_ff @ (posedge clock)
         if (reset)
             shift_reg <= 1;
         else if (enable)
             shift_reg <= { button_on, shift_reg [7:1] };
+            // si el boton se presiona se enciende button_on y al hacer el { button_on, shift_reg [7:1] }
+            // se esta colocando ese 1 del encendido en la posicion mas significativa de shift_reg
+            // y los demas valores del 7 al 1 ignorando el 0 se colocan a continuacion, eso seria como
+            // hacer un corrimiento hacia la derecha agregando un 1 desde la parte mas significativa.
             // Alternatively you can write:
             // shift_reg <= (button_on << 7) | (shift_reg >> 1);
+    */
 
-    assign led = shift_reg;
+
 
     // Exercise 1: Make the light move in the opposite direction.
+   
+   /*
+    always_ff @ (posedge clock)
+        if (reset)
+            shift_reg <= 1;
+        else if (enable)
+            shift_reg <= { shift_reg[6:0], button_on };
+            // si queremos lo contrario, se puede hacer lo siguiente { shift_reg[6:0], button_on };
+            // aqui colocamos en shift_reg primero los valores del 6 al 0 ignorando el mas significativo osea el 7
+            // estos iniciarian desde la parte mas significativa osea se recorrieron un lugar hacia la izquierda
+            // y en la posicion menos signiticativa se coloca el valor del boton si esta encendido o no.
+    */
+
 
     // Exercise 2: Make the light moving in a loop.
     // Use another key to reset the moving lights back to no lights.
+/*
+    //hacia la derecha
+    always_ff @ (posedge clock)
+        if (reset)
+            shift_reg <= 8'b00000001;
+        else if (enable)
+            shift_reg <= { shift_reg [0], shift_reg [7:1] };
+*/
+///*
+    //hacia la izquierda
+    always_ff @ (posedge clock)
+        if (reset)
+            shift_reg <= 8'b10000000;
+        else if (enable)
+            shift_reg <= { shift_reg [6:0], shift_reg [7] };
+//*/
+
+    assign led = shift_reg;
 
     // Exercise 3: Display the state of the shift register
     // on a seven-segment display, moving the light in a circle.
 
+    //   --a--
+    //  |     |
+    //  f     b
+    //  |     |
+    //   --g--
+    //  |     |
+    //  e     c
+    //  |     |
+    //   --d--  h
+
+    always_comb begin
+        case (shift_reg)
+                                    // abcdefgh
+            8'b00000001: abcdefgh = 8'b11111100; // 0
+            8'b00000010: abcdefgh = 8'b01100000; // 1
+            8'b00000100: abcdefgh = 8'b11011010; // 2
+            8'b00001000: abcdefgh = 8'b11110010; // 3
+            8'b00010000: abcdefgh = 8'b01100110; // 4
+            8'b00100000: abcdefgh = 8'b10110110; // 5
+            8'b01000000: abcdefgh = 8'b10111110; // 6
+            8'b10000000: abcdefgh = 8'b11100000; // 7
+            default: abcdefgh = 8'b00000000; // Apagado
+        endcase
+    end
+
+    // Seleccionar el primer digito en el display de 7 segmentos
+    assign digit = 8'b00000001;
 endmodule
